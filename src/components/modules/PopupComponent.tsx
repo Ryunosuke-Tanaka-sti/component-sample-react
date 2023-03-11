3c7d091e17fc873f13fa90f0987d4f613dc2d21f
ReiRyuSTI <ry-tanaka@sios.com>
2023-03-11 12:34:06 +0900

    popup change

--- src/components/modules/PopupComponent.tsx
+++ src/components/modules/PopupComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { memo, useEffect } from 'react';
 
 import { RxCross1 } from 'react-icons/rx';
 
@@ -7,43 +7,37 @@ type Props = {
   setViewFlag: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-export const PopUpComponent = (props: Props) => {
+export const PopUpComponent = memo((props: Props) => {
   const { viewFlag, setViewFlag } = props;
   useEffect(() => {
-    // 画面固定用関数
+    // 背景画面固定用関数
     const registerBackgroundFixed = () => {
       const body = document.body;
       const scrollWidth = window.innerWidth - body.clientWidth;
       body.style.marginRight = `${scrollWidth}px`;
       body.style.overflowY = 'hidden';
     };
-
-    // 画面固定解除用関数
+    // 背景画面固定解除用関数
     const unRegisterBackgroundFixed = () => {
       const body = document.body;
       body.style.overflowY = '';
       body.style.marginRight = '';
     };
-
     if (viewFlag) registerBackgroundFixed();
+
     return () => {
       unRegisterBackgroundFixed();
     };
   }, [viewFlag]);
 
   // 枠外クリック用関数
-  const onClickBackground = (
-    e: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
-    if (e.currentTarget === e.target) {
-      setViewFlag(false);
-    }
+  const onClickBackground = () => {
+    setViewFlag(false);
   };
 
-  // バツボタンクリック用関数
-  const onClickCross = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  // 枠内クリック
+  const onClickCard = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.stopPropagation();
-    setViewFlag(false);
   };
 
   return (
@@ -59,17 +53,18 @@ export const PopUpComponent = (props: Props) => {
       >
         <div className="relative h-3/4 w-3/4 max-w-3xl">
           {/* バツボタン */}
-          <div
-            className="absolute right-0 -top-10 h-10 w-10 hover:cursor-pointer"
-            onClick={onClickCross}
-          >
+          <div className="absolute right-0 -top-10 h-10 w-10 hover:cursor-pointer">
             <RxCross1 className="h-full w-full" />
           </div>
-          <div id="policy" className="flex h-full w-full flex-col bg-white">
+          <div
+            id="policy"
+            className="flex h-full w-full flex-col bg-white"
+            onClick={onClickCard}
+          >
             ここに何かしらを書く
           </div>
         </div>
       </div>
     </>
   );
-};
+});
